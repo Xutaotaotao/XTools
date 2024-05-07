ad9b6c34dcb4bca2e9d6ecd3fa2ee47c306d35a8
Xutaotaotao <xutaotao1108@outlook.com>
2024-05-07 20:15:51 +0800

    keywords

--- .umirc.ts
+++ .umirc.ts
@@ -5,4 +5,9 @@ export default defineConfig({
   base: '/XTools/',
   publicPath: '/XTools/',
   exportStatic: {},
+  metas:[
+    {
+      keywords:'XTools,xtools,xutaotao,图片压缩,图片切割,Json格式化,在线工具,前端工具,前端开发,专注工具，助力高效,完全本地化工具,无需云端,不担心隐私泄露'
+    }
+  ]
 });
