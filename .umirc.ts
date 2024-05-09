e199660ff49397295c898cf3d1a3b115caa923ba
Xutaotaotao <xutaotao1108@outlook.com>
2024-05-09 17:12:52 +0800

    esbuildMinifyIIFE

--- .umirc.ts
+++ .umirc.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   base: '/XTools/',
   publicPath: '/XTools/',
   exportStatic: {},
+  esbuildMinifyIIFE: true,
   metas:[
     {
       keywords:'XTools,xtools,xutaotao,图片压缩,图片切割,Json格式化,在线工具,前端工具,前端开发,专注工具，助力高效,完全本地化工具,无需云端,不担心隐私泄露'
